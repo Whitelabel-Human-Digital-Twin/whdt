a849eae824a1e3c0676b4770056195dd7813a0b3
LEONARDO MICELLI <leonardomicelli@gmail.com>
2025-06-06 10:23:03 +0200

    feat: add blood oxygen, mood and steps to model

---
+++ whdt-core/src/main/kotlin/hdt/model/BloodOxygen.kt
@@ -0,0 +1,45 @@
+package hdt.model
+
+import com.google.gson.Gson
+import hdt.util.Default
+import hdt.util.Deserialize
+import hdt.util.Serialize
+
+data class BloodOxygen(
+    val percentage: Int,
+    override val name: String = "Blood Oxygen",
+    override val internalName: String = "blood-oxygen",
+    override val description: String = "The level of oxygen in the blood.",
+    override val id: String = "loinc:2708-6", // LOINC code for oxygen saturation measurement
+    override val timestamp: Long = System.currentTimeMillis(),
+    val timezone: Int = 0,
+) : InstantProperty {
+
+    override fun defaultValue(): Default {
+        return default()
+    }
+
+    override fun deserialize(value: String): Deserialize {
+        return fromJson(value)
+    }
+
+    override fun serialize(s: Serialize): String {
+        return toJson(s as BloodOxygen)
+    }
+
+    companion object {
+        private val gson = Gson()
+
+        fun default(): BloodOxygen {
+            return BloodOxygen(95)
+        }
+
+        fun fromJson(json: String): BloodOxygen {
+            return gson.fromJson(json, BloodOxygen::class.java)
+        }
+
+        fun toJson(bloodOxygen: BloodOxygen): String {
+            return gson.toJson(bloodOxygen)
+        }
+    }
+}
---
+++ whdt-core/src/main/kotlin/hdt/model/Steps.kt
@@ -0,0 +1,44 @@
+package hdt.model
+
+import com.google.gson.Gson
+import hdt.util.Default
+import hdt.util.Deserialize
+import hdt.util.Serialize
+
+data class Steps(
+    val count: Int,
+    override val name: String = "Steps",
+    override val internalName: String = "steps",
+    override val description: String = "The number of steps taken.",
+    override val id: String = "hdt:00002-1", // custom code for step count
+    override val startTime: Long = System.currentTimeMillis(),
+    override val endTime: Long = System.currentTimeMillis(),
+): TimeRangedProperty {
+    override fun defaultValue(): Default {
+        return default()
+    }
+
+    override fun deserialize(value: String): Deserialize {
+        return fromJson(value)
+    }
+
+    override fun serialize(s: Serialize): String {
+        return toJson(s as Steps)
+    }
+
+    companion object {
+        private val gson = Gson()
+
+        fun default(): Steps {
+            return Steps(0)
+        }
+
+        fun fromJson(json: String): Steps {
+            return gson.fromJson(json, Steps::class.java)
+        }
+
+        fun toJson(steps: Steps): String {
+            return gson.toJson(steps)
+        }
+    }
+}
