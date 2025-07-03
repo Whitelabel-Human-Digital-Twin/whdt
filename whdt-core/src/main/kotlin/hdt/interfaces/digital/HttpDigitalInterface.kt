b6e00748b1ec648ceb2c60f7c403061698d6b7a4
Leonardo Micelli <leonardomicelli@gmail.com>
2025-07-03 16:43:07 +0200

    fix: add missing id to interface

--- whdt-core/src/main/kotlin/hdt/interfaces/digital/HttpDigitalInterface.kt
+++ whdt-core/src/main/kotlin/hdt/interfaces/digital/HttpDigitalInterface.kt
@@ -6,6 +6,7 @@ class HttpDigitalInterface(
     override val properties: List<Property>,
     val host: String = "localhost",
     val port: Int = 8080,
+    val id: String = "http-di-${System.currentTimeMillis()}",
 ) : DigitalInterface  {
     override val type: DigitalInterfaceType = DigitalInterfaceType.HTTP
 }
