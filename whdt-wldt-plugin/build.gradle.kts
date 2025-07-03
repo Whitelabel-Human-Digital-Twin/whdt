40f8f4bcc6139da102947e9dc84223deb816d629
Leonardo Micelli <leonardomicelli@gmail.com>
2025-07-03 16:21:44 +0200

    chore(deps): add http digital adapter

--- whdt-wldt-plugin/build.gradle.kts
+++ whdt-wldt-plugin/build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
     implementation("io.github.wldt:wldt-core:0.4.0")
     implementation("io.github.wldt:mqtt-physical-adapter:0.1.2")
     implementation("io.github.wldt:mqtt-digital-adapter:0.1.2")
+    implementation("io.github.wldt:http-digital-adapter:0.2")
 }
 
 tasks.test {
