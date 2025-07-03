fd7561b137cb8ec47779a3704471375bec7a8bb6
Leonardo Micelli <leonardomicelli@gmail.com>
2025-07-03 16:20:35 +0200

    refactor: move interfaces to their own file

--- whdt-core/src/main/kotlin/hdt/interfaces/digital/DigitalInterface.kt
+++ whdt-core/src/main/kotlin/hdt/interfaces/digital/DigitalInterface.kt
@@ -13,14 +13,3 @@ interface DigitalInterface {
     val type: DigitalInterfaceType
     val properties: List<Property>
 }
-
-class MqttDigitalInterface(
-    override val properties: List<Property>,
-    val broker: String = "localhost",
-    val port: Int = 1883,
-    val clientId: String = "mqtt-di-${System.currentTimeMillis()}"
-) : DigitalInterface {
-    override val type: DigitalInterfaceType = DigitalInterfaceType.MQTT
-
-    // Additional MQTT-specific properties or methods can be added here
-}
--- whdt-core/src/main/kotlin/hdt/interfaces/physical/PhysicalInterface.kt
+++ whdt-core/src/main/kotlin/hdt/interfaces/physical/PhysicalInterface.kt
@@ -10,14 +10,3 @@ interface PhysicalInterface {
     val properties: List<Property>
     val type: PhysicalInterfaceType
 }
-
-class MqttPhysicalInterface(
-    override val properties: List<Property>,
-    val broker: String = "localhost",
-    val port: Int = 1883,
-    val clientId: String = "mqtt-pa-${System.currentTimeMillis()}"
-) : PhysicalInterface {
-    override val type: PhysicalInterfaceType = PhysicalInterfaceType.MQTT
-
-    // Additional MQTT-specific properties or methods can be added here
-}
