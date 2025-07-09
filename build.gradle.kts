118c1303ea6eb2427c0210bc40e225dd2bfcd6d4
Leonardo Micelli <leonardomicelli@gmail.com>
2025-07-09 17:24:53 +0200

    test: add default test

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,23 @@ dependencies {
     testImplementation(kotlin("test"))
 }
 
+subprojects {
+    // Optional: shared dependency versions
+    val kotestVersion = "5.8.0"
+
+    plugins.withId("org.jetbrains.kotlin.jvm") {
+        dependencies {
+            "testImplementation"("io.kotest:kotest-runner-junit5:$kotestVersion")
+            "testImplementation"("io.kotest:kotest-assertions-core:$kotestVersion")
+            "testImplementation"("io.kotest:kotest-framework-engine:$kotestVersion")
+        }
+
+        tasks.withType<Test> {
+            useJUnitPlatform()
+        }
+    }
+}
+
 tasks.test {
     useJUnitPlatform()
 }
