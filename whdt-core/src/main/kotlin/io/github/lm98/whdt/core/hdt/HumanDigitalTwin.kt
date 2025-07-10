dee5b9a530170584012fc3d227cfd041eb1a7728
Leonardo Micelli <leonardomicelli@gmail.com>
2025-07-10 19:25:33 +0200

    chore(docs): add comment to hdt

--- whdt-core/src/main/kotlin/io/github/lm98/whdt/core/hdt/HumanDigitalTwin.kt
+++ whdt-core/src/main/kotlin/io/github/lm98/whdt/core/hdt/HumanDigitalTwin.kt
@@ -5,6 +5,9 @@ import io.github.lm98.whdt.core.hdt.interfaces.physical.PhysicalInterface
 import io.github.lm98.whdt.core.hdt.model.Model
 import kotlinx.serialization.Serializable
 
+/**
+ * Represents a Human Digital Twin (HDT) which is a digital representation of a human being.
+ */
 @Serializable
 data class HumanDigitalTwin(
     val id: String,
