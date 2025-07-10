f37728f99fcea110ea32549319c660a2c1d43018
Leonardo Micelli <leonardomicelli@gmail.com>
2025-07-10 18:27:29 +0200

    test: test serde model

--- whdt-core/src/test/kotlin/io/github/lm98/whdt/core/serde/TestSerDeProperty.kt
+++ whdt-core/src/test/kotlin/io/github/lm98/whdt/core/serde/TestSerDeProperty.kt
@@ -1,9 +1,9 @@
 package io.github.lm98.whdt.core.serde
 
 import io.github.lm98.whdt.core.hdt.model.Model
-import io.github.lm98.whdt.core.hdt.model.property.PropertyValue
 import io.github.lm98.whdt.core.hdt.model.property.GenericProperty
 import io.github.lm98.whdt.core.hdt.model.property.Property
+import io.github.lm98.whdt.core.hdt.model.property.PropertyValue
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import kotlinx.serialization.json.Json
@@ -22,5 +22,23 @@ class TestSerDeProperty: FunSpec({
         val deserialized = json.decodeFromString<Property>(serialized)
 
         deserialized shouldBe prop
+    }
+
+    test("Test SerDe Model") {
+        val json = Json {
+            serializersModule = io.github.lm98.whdt.core.serde.modules.propertyModule
+            classDiscriminator = "type"
+            prettyPrint = true
+        }
+
+        val model = Model(listOf(
+            GenericProperty("username", "username", value = PropertyValue.StringPropertyValue("leona")),
+            GenericProperty("password", "password", value = PropertyValue.StringPropertyValue("123456")),
+        ))
+        val serialized = json.encodeToString(Model.serializer(), model)
+        //println(serialized)
+        val deserialized = json.decodeFromString(Model.serializer(),serialized)
+
+        deserialized shouldBe model
     }
 })
