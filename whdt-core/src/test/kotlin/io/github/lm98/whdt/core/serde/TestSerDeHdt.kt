53219b331ee7a3cc45cd314f940c9d20ec5aeac8
Leonardo Micelli <leonardomicelli@gmail.com>
2025-07-14 11:28:27 +0200

    feat: add stub for serialization/deserialization

--- whdt-core/src/test/kotlin/io/github/lm98/whdt/core/serde/TestSerDeHdt.kt
+++ whdt-core/src/test/kotlin/io/github/lm98/whdt/core/serde/TestSerDeHdt.kt
@@ -8,19 +8,11 @@ import io.github.lm98.whdt.core.hdt.model.property.BloodPressure
 import io.github.lm98.whdt.core.hdt.model.property.GivenName
 import io.github.lm98.whdt.core.hdt.model.property.HeartRate
 import io.github.lm98.whdt.core.hdt.model.property.Surname
-import io.github.lm98.whdt.core.serde.modules.hdtModule
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
-import kotlinx.serialization.json.Json
 
 class TestSerDeHdt: FunSpec({
   test("Test SerDe HumanDigitalTwin") {
-      val json = Json {
-          serializersModule = hdtModule
-          classDiscriminator = "type"
-          prettyPrint = true
-      }
-
       val properties = listOf(
           GivenName("John"),
           Surname("Doe"),
@@ -46,9 +38,9 @@ class TestSerDeHdt: FunSpec({
           digitalInterfaces = listOf(dI),
       )
 
-      val serialized = json.encodeToString(hdt)
-      //println(serialized)
-      val deserialized = json.decodeFromString< HumanDigitalTwin>(serialized)
+      val serialized = Stub.hdtJsonSerDe().serialize(hdt)
+      println(serialized)
+      val deserialized = Stub.hdtJsonSerDe().deserialize(serialized)
 
       deserialized shouldBe hdt
   }
