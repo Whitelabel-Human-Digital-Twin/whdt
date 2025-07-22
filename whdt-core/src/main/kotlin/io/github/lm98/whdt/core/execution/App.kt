e61c1d7d9bd5282fd05a827d91051662616aeb44
LEONARDO MICELLI <leonardomicelli@gmail.com>
2025-07-22 16:33:18 +0200

    refactor(core): refactor App to encapsulate operation failure

--- whdt-core/src/main/kotlin/io/github/lm98/whdt/core/execution/App.kt
+++ whdt-core/src/main/kotlin/io/github/lm98/whdt/core/execution/App.kt
@@ -3,27 +3,28 @@ package io.github.lm98.whdt.core.execution
 import io.github.lm98.whdt.core.hdt.HumanDigitalTwin
 
 interface App {
-    fun addDt(hdt: HumanDigitalTwin): App
-    fun addDts(hdts: List<HumanDigitalTwin>): App
-    fun removeDtById(id: String): App
-    fun removeDtsById(ids: List<String>): App
-
-    fun removeDt(hdt: HumanDigitalTwin): App {
+    fun addDt(hdt: HumanDigitalTwin): Result<Unit>
+    fun addDts(hdts: List<HumanDigitalTwin>): List<Result<Unit>> {
+        return hdts.map { addDt(it) }
+    }
+    fun removeDtById(id: String): Result<Unit>
+    fun removeDtsById(ids: List<String>): List<Result<Unit>> {
+        return ids.map { removeDtById(it)  }
+    }
+    fun removeDt(hdt: HumanDigitalTwin): Result<Unit> {
         return removeDtById(hdt.id)
     }
-    fun removeDts(hdts: List<HumanDigitalTwin>): App {
+    fun removeDts(hdts: List<HumanDigitalTwin>): List<Result<Unit>> {
         return removeDtsById(hdts.map { it.id })
     }
-    fun startDt(id: String): App
-    fun startDts(ids: List<String>): App {
-        ids.forEach { startDt(it) }
-        return this
+    fun startDt(id: String): Result<Unit>
+    fun startDts(ids: List<String>): List<Result<Unit>> {
+        return ids.map { startDt(it) }
     }
-    fun stopDt(id: String): App
-    fun stopDts(ids: List<String>): App {
-        ids.forEach { stopDt(it) }
-        return this
+    fun stopDt(id: String): Result<Unit>
+    fun stopDts(ids: List<String>): List<Result<Unit>> {
+        return ids.map { stopDt(it) }
     }
-    fun startAll()
-    fun stopAll()
+    fun startAll(): Result<Unit>
+    fun stopAll(): Result<Unit>
 }
