06d166ca03cd8dc74b1a4e74d28357edd87a5620
Macca1508 <t.maccaroni@uniurb.campus.it>
2025-07-31 14:27:43 +0200

    aggiustamennto test

--- whdt-csv-parser/src/test/kotlin/io/github/lm98/whdt/csv/parser/ParserCSVTest.kt
+++ whdt-csv-parser/src/test/kotlin/io/github/lm98/whdt/csv/parser/ParserCSVTest.kt
@@ -1,6 +1,7 @@
 package io.github.lm98.whdt.csv.parser
 
 import io.github.lm98.whdt.core.hdt.model.property.PropertyValue
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
 
 class ParserCSVTest: FunSpec({
@@ -32,6 +33,8 @@ class ParserCSVTest: FunSpec({
 
     test("TestSizeException") {
         val parser = ParserCSV.createParserCSV()
-        val p = parser.parsing("# paziente;età gestazionale (weeks);sex\n1;28.3").get(1)?.get(2)?.component5()
+        val exception = shouldThrow<SizeException> {
+            parser.parsing("# paziente;età gestazionale (weeks);sex\n1;28.3")
+        }
     }
 })
