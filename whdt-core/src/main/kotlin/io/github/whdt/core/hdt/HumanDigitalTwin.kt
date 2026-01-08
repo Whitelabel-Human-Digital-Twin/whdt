f1559abd0420ca8269f126c89982c39daf824b02
LEONARDO MICELLI <leonardomicelli@gmail.com>
2026-01-08 17:22:06 +0100

    refactor: refactor Storage logic

--- whdt-core/src/main/kotlin/io/github/whdt/core/hdt/HumanDigitalTwin.kt
+++ whdt-core/src/main/kotlin/io/github/whdt/core/hdt/HumanDigitalTwin.kt
@@ -6,6 +6,7 @@ import io.github.whdt.core.hdt.model.Model
 import io.github.whdt.core.hdt.storage.Storage
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 
 /**
  * Represents a Human Digital Twin (HDT), a digital representation of a human being.
@@ -17,5 +18,5 @@ data class HumanDigitalTwin(
     val models: List<Model> = emptyList(),
     val physicalInterfaces: List<PhysicalInterface> = emptyList(),
     val digitalInterfaces: List<DigitalInterface> = emptyList(),
-    val storages: List<Storage> = listOf(Storage.IN_MEMORY),
+    val storages: List<Storage> = listOf(Storage.default()),
 )
--- whdt-core/src/main/kotlin/io/github/whdt/core/hdt/storage/Storage.kt
+++ whdt-core/src/main/kotlin/io/github/whdt/core/hdt/storage/Storage.kt
@@ -1,8 +1,43 @@
 package io.github.whdt.core.hdt.storage
 
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
 @Serializable
-enum class Storage {
+enum class StorageType {
     IN_MEMORY,
+    DB_MONGO,
+    DB_POSTGRESQL,
+}
+
+@Serializable
+@OptIn(ExperimentalUuidApi::class)
+data class Storage(
+    val id: String,
+    val storageType: StorageType,
+    @Transient
+    val config: Map<String, String> = emptyMap(),
+) {
+    fun addConfig(c: Map<String, String>): Storage {
+        return copy(config = config + c)
+    }
+
+    fun addConfigProperty(p: Pair<String, String>): Storage {
+        return copy(config = config.plus(p))
+    }
+
+    fun removeConfigProperty(p: String): Storage {
+        return copy(config = config.minus(p))
+    }
+
+    companion object {
+        fun default(): Storage {
+            return Storage(
+                id = Uuid.random().toString(),
+                storageType = StorageType.IN_MEMORY,
+            )
+        }
+    }
 }
