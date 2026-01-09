6062faa316eaad077357bbcb07b4ea4a2e1f5d3f
lm98 <leonardomicelli@gmail.com>
2026-01-09 17:37:17 +0100

    chore(build): add whdt-distributed

---
+++ whdt-distributed/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    kotlin("jvm")
+    `maven-publish`
+}
+
+val moduleVersion: String = rootProject.file("${project.name}/version.txt").readText().trim()
+
+publishing {
+    publications {
+        create<MavenPublication>("gpr") {
+            from(components["java"])
+            groupId = "io.github.whdt"
+            artifactId = project.name
+            version = moduleVersion
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/Whitelabel-Human-Digital-Twin/whdt")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("GPR_USER")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("GPR_KEY")
+            }
+        }
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation(project(":whdt-core"))
+}
+
+kotlin {
+    jvmToolchain(23)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
