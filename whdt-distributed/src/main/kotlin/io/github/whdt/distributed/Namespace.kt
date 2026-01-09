ce9340f4b61cee16e138c63818abb19e1af00d65
lm98 <leonardomicelli@gmail.com>
2026-01-09 17:38:04 +0100

    feat: add namespace for mqtt topics

---
+++ whdt-distributed/src/main/kotlin/io/github/whdt/distributed/Namespace.kt
@@ -0,0 +1,17 @@
+package io.github.whdt.distributed
+
+object Namespace {
+    const val PROPERTY_TOPIC_MQTT = "whdt-property"
+
+    val namespaces = listOf(
+        PROPERTY_TOPIC_MQTT,
+        )
+
+    fun verifyNamespace(namespace: String): Result<Unit> {
+        return if (namespaces.contains(namespace)) {
+            Result.success(Unit)
+        } else {
+            Result.failure(IllegalArgumentException("Namespace $namespace does not exist."))
+        }
+    }
+}
