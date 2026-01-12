54886146f6c83af9eec9b4c6720341582219ee7d
lm98 <leonardomicelli@gmail.com>
2026-01-12 17:08:41 +0100

    feat: add timestamps to message

--- whdt-distributed/src/main/kotlin/io/github/whdt/distributed/message/Message.kt
+++ whdt-distributed/src/main/kotlin/io/github/whdt/distributed/message/Message.kt
@@ -5,10 +5,26 @@ import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.decodeFromJsonElement
+import kotlin.time.Clock
+import kotlin.time.ExperimentalTime
+import kotlin.time.Instant
+
 
 @Serializable
-data class Message(val namespace: String, val id: HdtIdentifier, val payload: JsonElement) {
+@OptIn(ExperimentalTime::class)
+data class Message(
+    val namespace: String,
+    val id: HdtIdentifier,
+    val sentAt: Instant,
+    @Transient
+    val receivedAt: Instant = Clock.System.now(),
+    val payload: JsonElement
+) {
 
+    /**
+     * @param T the expected type for payload
+     * @return the Message's deserialized payload, wrapped in a [Result]
+     */
     inline fun<reified T> unwrap(): Result<T> {
         return runCatching {
             Json.decodeFromJsonElement(payload)
