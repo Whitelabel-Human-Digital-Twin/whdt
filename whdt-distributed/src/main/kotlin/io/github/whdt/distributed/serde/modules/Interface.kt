259f83cbed6efa86847a1bc25b4ba8614127f7a8
lm98 <leonardomicelli@gmail.com>
2026-02-26 11:25:51 +0100

    feat: add generic dt interface implementations

--- whdt-distributed/src/main/kotlin/io/github/whdt/distributed/serde/modules/Interface.kt
+++ whdt-distributed/src/main/kotlin/io/github/whdt/distributed/serde/modules/Interface.kt
@@ -1,20 +1,24 @@
 package io.github.whdt.distributed.serde.modules
 
 import io.github.whdt.core.hdt.interfaces.digital.DigitalInterface
+import io.github.whdt.core.hdt.interfaces.digital.DigitalInterfaceImpl
 import io.github.whdt.core.hdt.interfaces.digital.HttpDigitalInterface
 import io.github.whdt.core.hdt.interfaces.digital.MqttDigitalInterface
 import io.github.whdt.core.hdt.interfaces.physical.MqttPhysicalInterface
 import io.github.whdt.core.hdt.interfaces.physical.PhysicalInterface
+import io.github.whdt.core.hdt.interfaces.physical.PhysicalInterfaceImpl
 import kotlinx.serialization.modules.SerializersModule
 import kotlinx.serialization.modules.polymorphic
 
 val interfaceModule = SerializersModule {
     polymorphic(PhysicalInterface::class) {
         subclass(MqttPhysicalInterface::class, MqttPhysicalInterface.serializer())
+        subclass(PhysicalInterfaceImpl::class, PhysicalInterfaceImpl.serializer())
     }
 
     polymorphic(DigitalInterface::class) {
         subclass(HttpDigitalInterface::class, HttpDigitalInterface.serializer())
         subclass(MqttDigitalInterface::class, MqttDigitalInterface.serializer())
+        subclass(DigitalInterfaceImpl::class, DigitalInterfaceImpl.serializer())
     }
 }
