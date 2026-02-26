81263a1352d45ca61ecbdedaf964862b5c63680a
lm98 <leonardomicelli@gmail.com>
2026-02-26 11:16:35 +0100

    fix(build): add publishing

--- whdt-dsl/build.gradle.kts
+++ whdt-dsl/build.gradle.kts
@@ -1,9 +1,31 @@
 plugins {
     kotlin("jvm")
+    `maven-publish`
 }
 
-group = "io.github.whdt"
-version = "0.0.0-SNAPSHOT"
+val moduleVersion: String = rootProject.file("${project.name}/version.txt").readText().trim()
+
+publishing {
+    publications {
+        create<MavenPublication>("gpr") {
+            from(components["java"])
+            groupId = "io.github.whdt"
+            artifactId = project.name
+            version = moduleVersion
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/Whitelabel-Human-Digital-Twin/whdt")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("GPR_USER")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("GPR_KEY")
+            }
+        }
+    }
+}
 
 repositories {
     mavenCentral()
