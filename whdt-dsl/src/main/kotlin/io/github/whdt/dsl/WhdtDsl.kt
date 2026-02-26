4dbd5e7b4e4897ef8fcad380c07b8c6965b62317
lm98 <leonardomicelli@gmail.com>
2026-02-26 11:38:21 +0100

    feat: add dsl entry point

--- whdt-dsl/src/main/kotlin/io/github/whdt/dsl/WhdtDsl.kt
+++ whdt-dsl/src/main/kotlin/io/github/whdt/dsl/WhdtDsl.kt
@@ -1,4 +1,13 @@
 package io.github.whdt.dsl
 
+import io.github.whdt.core.hdt.HumanDigitalTwin
+import io.github.whdt.core.hdt.model.id.HdtId
+import io.github.whdt.dsl.builder.HumanDigitalTwinBuilder
+
 @DslMarker
 annotation class WhdtDsl
+
+fun hdt(hdtIdRaw: String, block: HumanDigitalTwinBuilder.() -> Unit): HumanDigitalTwin {
+    val hdtId = HdtId(hdtIdRaw) // adapt to your actual type
+    return HumanDigitalTwinBuilder(hdtId).apply(block).build()
+}
