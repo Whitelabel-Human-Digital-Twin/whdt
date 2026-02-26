de653067777d2ec70b73a7a2ad7f812397ef9da6
lm98 <leonardomicelli@gmail.com>
2026-02-26 12:12:13 +0100

    chore(build): add logback to all subprojects

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,10 +18,12 @@ dependencies {
 subprojects {
     // Optional: shared dependency versions
     val kotestVersion = "5.8.0"
+    val logbackVersion = "1.4.14"
 
     plugins.withId("org.jetbrains.kotlin.jvm") {
         dependencies {
             "implementation"("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+            "implementation"("ch.qos.logback:logback-classic:$logbackVersion")
             "testImplementation"("io.kotest:kotest-runner-junit5:$kotestVersion")
             "testImplementation"("io.kotest:kotest-assertions-core:$kotestVersion")
             "testImplementation"("io.kotest:kotest-framework-engine:$kotestVersion")
