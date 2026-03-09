9e609a85bdf91fe69352b494358fed6eac582b47
lm98 <leonardomicelli@gmail.com>
2026-03-09 12:41:03 +0100

    refactor: use HdtId

--- whdt-core/src/main/kotlin/io/github/whdt/core/hdt/model/Model.kt
+++ whdt-core/src/main/kotlin/io/github/whdt/core/hdt/model/Model.kt
@@ -1,5 +1,6 @@
 package io.github.whdt.core.hdt.model
 
+import io.github.whdt.core.hdt.model.id.HdtId
 import io.github.whdt.core.hdt.model.property.Property
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
@@ -7,7 +8,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 @SerialName("model")
 data class Model(
-    val hdtId: String,
+    val hdtId: HdtId,
     val name: String,
     val description: String,
     val properties: List<Property>,
