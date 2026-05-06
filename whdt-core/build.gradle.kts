57cd71061171c04f95244bd606a39681f4ff94f9
lm98 <leonardomicelli@gmail.com>
2026-05-06 15:21:38 +0200

    fix(core): remove unused dependency

--- whdt-core/build.gradle.kts
+++ whdt-core/build.gradle.kts
@@ -35,7 +35,6 @@ dependencies {
     testImplementation("io.kotest:kotest-runner-junit5:5.8.0") // Required to run tests
     testImplementation("io.kotest:kotest-assertions-core:5.8.0") // Optional: assertions
     testImplementation("io.kotest:kotest-framework-engine:5.8.0") // Optional but useful
-    implementation("com.google.code.gson:gson:2.13.1")
 }
 
 tasks.test {
